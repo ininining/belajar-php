b19422af8795ce195bd76c2d94a07d2a40329de3
Sri Mulya Ningsih <muningsri727@gmail.com>
2022-10-24 10:56:01 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    echo "Welcome!<br>";
+
+    $nama = "Sri Mulya Ningsih";
+    $umur = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun. <br>";
+
+    $namaAdik = "Ayu";
+    $umurAdik = 19;
+
+    echo "Nama adik saya <storng>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+    $selisihUmur = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun";
+
+
+?>
